0e6e79efce253087dd362b1f9e60e48e82dc2b60
LauwXiang <474251861@qq.com>
2017-11-11 15:05:55 +0800

    lau

---
+++ mobile/src/modules/buycar/BuycarActions.js
@@ -0,0 +1,13 @@
+import * as constants from '../../redux/commonConstant'
+
+export function login(username, password){
+    return {
+        types: [constants.REQUEST, constants.SUCCESS, constants.FAILURE],
+        path: 'login',
+        method: 'post',
+        query: {username, password}
+    }
+    // return {
+    //  type: constants.REQUEST
+    // }
+}
--- mobile/src/router.js
+++ mobile/src/router.js
@@ -3,9 +3,11 @@ import {Route} from 'react-router'
 
 import AppComponent from './modules/app/AppComponent'
 import LoginComponent from './modules/login/LoginComponent'
+import BuycarComponent from './modules/buycar/BuycarComponent'
 
 export default (
     <Route path="/" component={AppComponent}>
         <Route path="login" component={LoginComponent} />
+        <Route path="buycar" component={BuycarComponent} />
     </Route>
 )
