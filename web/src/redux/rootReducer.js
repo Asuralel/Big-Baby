4714137c39c2be0e6002a1e7c678925f94a236fa
huangxinglian <942533134@qq.com>
2017-11-11 15:09:54 +0800

    h

--- web/src/redux/rootReducer.js
+++ web/src/redux/rootReducer.js
@@ -1,11 +1,14 @@
-import login from '../modules/login/LoginReducer'
+import login from '../modules/login/LoginReducer';
 import {routerReducer as router} from 'react-router-redux'
 
-import {combineReducers} from 'redux'
+import {combineReducers} from 'redux';
+import test from '../modules/test/TestReducer';
+
 
 const rootReducer = combineReducers({
     login,
-    router
+    router,
+    test
 })
 
 export default rootReducer
--- web/src/router.js
+++ web/src/router.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import {Route} from 'react-router'
+import React from 'react';
+import {Route,IndexRoute} from 'react-router';
 
-import AppComponent from './modules/app/AppComponent'
-import LoginComponent from './modules/login/LoginComponent'
+import AppComponent from './modules/app/AppComponent';
+import LoginComponent from './modules/login/LoginComponent';
+import TestComponent from "./modules/test/TestComponent";
 
 export default (
     <Route path="/" component={AppComponent}>
+    	<IndexRoute />
         <Route path="login" component={LoginComponent} />
+        <Route path="test" component={TestComponent} />
     </Route>
 )
