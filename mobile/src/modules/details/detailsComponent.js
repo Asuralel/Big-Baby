20b6ab7670c143f2ff0c8ecf2faa9d5ed5b3df9c
chen wengui <1092204971@qq.com>
2017-11-13 09:05:48 +0800

    拉取

--- mobile/src/modules/details/detailsComponent.js
+++ mobile/src/modules/details/detailsComponent.js
@@ -1,7 +1,8 @@
 import React from 'react' 
 
+import './details.scss'
 
-import {Icon,Input} from 'antd';
+import {Icon,Input,BackTop,Carousel} from 'antd';
 const Search = Input.Search;
 
 class detailsComponent extends React.Component {
@@ -10,12 +11,41 @@ class detailsComponent extends React.Component {
             <div className="details">
                 <div className="detailst-head">
 					<Icon type="left" style={{ fontSize: 20, color: '#555555' }} />
-					<Search placeholder="请输入搜索关键字" style={{ width: 200 }} onSearch={value => console.log(value)} />
+					<ul>
+						<li>商品</li>
+						<li>详情</li>
+						<li>评论({0})</li>
+					</ul>
 					<Icon type="ellipsis"  style={{ fontSize: 20, color: '#555555' }}/>
 				</div>
 				<div className="details-cont">
-					详情页
+					<ul>
+						<li>
+							<Carousel autoplay>
+							    <div><h3>1</h3></div>
+							    <div><h3>2</h3></div>
+							    <div><h3>3</h3></div>
+							    <div><h3>4</h3></div>
+							</Carousel>
+						</li>
+						<li>详情</li>
+						<li>评论</li>
+					</ul>
 				</div>
+				<div className="details-footer" style={{height:'800px'}}>
+					<ul>
+						<li><Icon type="message"  style={{ fontSize: 20, color: '#555555' }}/>客服</li>
+						<li><Icon type="star-o"  style={{ fontSize: 20, color: '#555555' }}/>收藏</li>
+						<li><Icon type="shopping-cart"  style={{ fontSize: 20, color: '#555555' }}/>购物车</li>
+						<li>加入购物车</li>
+						<li>立即购买</li>
+					</ul>
+				</div>
+				<div>
+				    <BackTop>
+				      <div className="ant-back-top-inner"><Icon type="up-circle-o"  style={{ fontSize: 20, color: '#555555' }}/></div>
+				    </BackTop>
+				 </div>
             </div>
         )
     }
--- mobile/src/modules/list/listComponent.js
+++ mobile/src/modules/list/listComponent.js
@@ -1,4 +1,5 @@
-import React from 'react' 
+import React from 'react';
+import {Link} from 'react-router'
 
 
 import {Icon,Input} from 'antd';
@@ -9,12 +10,26 @@ class listComponent extends React.Component {
         return(
             <div className="list">
                 <div className="list-head">
-					<Icon type="left" style={{ fontSize: 20, color: '#555555' }} />
-					<Search placeholder="请输入搜索关键字" style={{ width: 200 }} onSearch={value => console.log(value)} />
-					<Icon type="ellipsis"  style={{ fontSize: 20, color: '#555555' }}/>
+					<div className="list-search">
+						<Icon type="left" style={{ fontSize: 20, color: '#555555' }} />
+						<Search placeholder="请输入搜索关键字" style={{ width: 200 }} onSearch={value => console.log(value)} />
+						<Link to="/sort" className="to-sort"><Icon type="bars"  style={{ fontSize: 20, color: '#555555' }}/>分类</Link>
+						<Icon type="ellipsis"  style={{ fontSize: 20, color: '#555555' }}/>
+					</div>
+					<div className="list-filter">
+						<ul>
+							<li>综合</li>
+							<li>销量</li>
+							<li>价格<Icon type="caret-up" /><Icon type="caret-down" /></li>
+							<li>筛选</li>
+							<li><Icon type="menu-unfold" /><Icon type="menu-fold" /></li>
+						</ul>
+					</div>
 				</div>
 				<div className="list-cont">
-					列表页
+					<ul>
+						<li>根据筛选条件动态生成</li>
+					</ul>
 				</div>
             </div>
         )
