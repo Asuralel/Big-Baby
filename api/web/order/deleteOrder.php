b2a37ed2f1d09abfa314b12db51d2807705b964f
lifaqiang <2210751283@qq.com>
2017-11-17 11:52:04 +0800

    我的订单

--- api/web/order/deleteOrder.php
+++ api/web/order/deleteOrder.php
@@ -5,8 +5,8 @@
     //允许所有域名发起的跨域请求，可以使用通配符 *
     $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
     // 编写查询sql语句
-    $sql = "delete from order where id='$id'";
-    echo $sql;
+    $sql = "delete from `order` where id='$id'";
+    // echo $sql;
     // 利用sql语句查询数据库
     // 查询结果集
     $result = $conn->query($sql);
