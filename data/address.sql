12b21e1ba9f89019aa17573a9cf631380376f19e
huangxinglian <942533134@qq.com>
2017-11-18 09:01:18 +0800

    添加数据库文件

---
+++ data/address.sql
@@ -0,0 +1,9 @@
+INSERT INTO `address` VALUES (29, 'laughing', '1888888888', '北京天安门', '大宝');
+INSERT INTO `address` VALUES (31, 'laughing', '188888888', '北京天安门', 'huangxinglian');
+INSERT INTO `address` VALUES (35, 'hxl123', '18877572889', '广州', '黄杏连');
+INSERT INTO `address` VALUES (36, 'a11111', '1fsfdsad', 'dsadsadas', 'zhangshengmao ');
+INSERT INTO `address` VALUES (37, 'a11111', 'sdsad', 'sadsa', 'sada');
+INSERT INTO `address` VALUES (38, 'gggggg', '12346757889', '广州', 'gggggg');
+INSERT INTO `address` VALUES (39, 'huang123', '18877572889', '广州', 'huang123');
+INSERT INTO `address` VALUES (40, 'huang123', '18877572889', '北京', 'huang123');
+INSERT INTO `address` VALUES (41, 'chenwengui', '18877572889', '北京天安门', 'chenwengui');
